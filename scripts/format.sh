778e5d4bcb5d9e7509c5f5eba4182425debbe269
Andrei Liviu Georgescu <andrei@andreigeorgescu.com>
2022-12-31 13:29:25 +0000

    a

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if [[ "$CI" == 'true' ]]; then
-  npx prettier --ignore-unknown --no-error-on-unmatched-pattern --check "$@"
-else
-  npx prettier --ignore-unknown --no-error-on-unmatched-pattern --write "$@"
-fi
+npx prettier --ignore-unknown --no-error-on-unmatched-pattern --check "$@"
