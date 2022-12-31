6a69ad8f7e4e1d19a7cb39f1dcb8e33642454ce9
Andrei Liviu Georgescu <andrei@andreigeorgescu.com>
2022-12-31 20:01:25 +0000

    a

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,11 +4,11 @@ for FILE in "$@"; do
   case "$FILE" in
 
     *.ts)
-      npx eslint --config .eslintrc.yaml --ignore-path .eslintignore --exit-on-fatal-error --max-warnings 0 --report-unused-disable-directives --no-error-on-unmatched-pattern "$FILE"
+      npx eslint --exit-on-fatal-error --max-warnings 0 --report-unused-disable-directives --no-error-on-unmatched-pattern "$FILE"
       ;;
 
     # *.ts)
-    # postcss --config path/to/your/config --replace 
+    # postcss --config path/to/your/config --replace
     # stylelint --syntax=scss
     # ;;
 
