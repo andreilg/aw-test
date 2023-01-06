d9bee9d3f72548c474805cfe0daadf9d8ad6fb31
Andrei Liviu Georgescu <andrei@andreigeorgescu.com>
2023-01-05 20:20:18 -0500

    a

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,14 +3,17 @@
 for FILE in "${@}"; do
   case "${FILE}" in
     *.sh)
+      echo 'hello'
       # shellcheck "${FILE}"
       ;;
 
     *.ts)
+      echo 'hello'
       # npx eslint --exit-on-fatal-error --max-warnings 0 --report-unused-disable-directives "${FILE}"
       ;;
 
     Dockerfile)
+      echo 'hello'
       # hadolint "${FILE}"
       ;;
 
