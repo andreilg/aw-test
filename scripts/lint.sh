d58812acb900dc04197b00357ddee859e9877878
Andrei Liviu Georgescu <andrei@andreigeorgescu.com>
2023-01-07 04:25:36 +0000

    a

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -15,7 +15,7 @@ for FILE in "${@}"; do
       # npx eslint --exit-on-fatal-error --max-warnings 0 --report-unused-disable-directives "${FILE}"
       ;;
 
-    "${PWD}"/**/*Dockerfile)
+    *Dockerfile)
       echo "${FILE}" > tst.txt
       hadolint "${FILE}"
       ;;
