1accedc9f1fc12ddcda592dfb148c0200b4c3f24
Andrei Liviu Georgescu <andrei@andreigeorgescu.com>
2023-01-12 12:56:09 +0000

    a

--- scripts/setup/build_ignores/build_gitignore.sh
+++ scripts/setup/build_ignores/build_gitignore.sh
@@ -6,6 +6,7 @@ rm .gitignore
     echo "$(< "${FILE}")"
     printf "\n"
   done
+  echo "$(< "${WORKDIR}".exceptions)"
 } >> .gitignore
-# Required to remove extra trailing newline due to above off-by-one error
-echo "$(< .gitignore)" > .gitignore
+# # Required to remove extra trailing newline due to above off-by-one error
+# echo "$(< .gitignore)" > .gitignore
